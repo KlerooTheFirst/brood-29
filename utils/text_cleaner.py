97521691437c8727407fd3bd08d6da81d27f63b8
leandro.mani <leandro.mani@liip.ch>
2026-06-26 10:34:26 +0200

    removing uneeded comment

--- utils/text_cleaner.py
+++ utils/text_cleaner.py
@@ -3,7 +3,6 @@ import string
 def filterText(text, criteria):
     return ''.join(ch for ch in text if ch not in criteria )
 
-# Replace the letter j with i and u with v (standard historical runic simplifications).
 def clean_input_text(raw_text):
     low_text = raw_text.lower()
     filtered = filterText(low_text,string.punctuation)
