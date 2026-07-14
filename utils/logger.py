5e1b6af2b5dd77df7b68fd80459576ead4f48ec3
leandro.mani <leandro.mani@liip.ch>
2026-07-14 17:37:31 +0200

    feat: added loggin

---
+++ utils/logger.py
@@ -0,0 +1,31 @@
+import logging
+from pathlib import Path
+
+LOG_FILE = Path("brood.log")
+
+
+def setup_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        fmt="[%(asctime)s] [%(levelname)s] [%(module)s]: %(message)s",
+        datefmt="%H:%M:%S",
+    )
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler(LOG_FILE, encoding="utf-8")
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    logger.propagate = False
+
+    return logger
